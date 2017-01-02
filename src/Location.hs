86f96bcb95edca3a7bb74e138e9e9618feba62db
Sam Protas <smprts@gmail.com>
2017-01-02 18:55:03 -0500

    Add lat/lon to Location

--- src/Location.hs
+++ src/Location.hs
@@ -5,20 +5,41 @@ module Location (
   Location (..)
 ) where
 
+import Text.Read
 import Data.Text
 import GHC.Generics
 import Data.Aeson
+import Data.Aeson.Types
 import Network.HTTP.Client (newManager)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Network.HTTP.Simple
 
 data Location = Location { city :: Text
                          , region :: Text
-                         , country :: Text }
+                         , country :: Text
+                         , latitude :: Float
+                         , longitude :: Float }
                          deriving (Show, Generic)
 
-instance FromJSON Location
 instance ToJSON Location
+instance FromJSON Location where
+  parseJSON (Object o) = do
+    parsedCity <- o .: "city"
+    parsedRegion <- o .: "region"
+    parsedCountry <- o .: "country"
+    location <- o .: "loc"
+    (parsedLat, parsedLong) <- locToLatLong location
+    return $ Location parsedCity parsedRegion parsedCountry parsedLat parsedLong
+    where
+      locToLatLong :: Text -> Parser (Float, Float)
+      locToLatLong loc = parseLatLong $ split (==',') loc
+      parseLatLong :: [Text] -> Parser (Float, Float)
+      parseLatLong [textLat, textLong] = case (,) <$> readMaybe (unpack textLat) <*> readMaybe (unpack textLong)
+          of Just parsed -> return parsed
+             _ -> parseFailure
+      parseLatLong _ = parseFailure
+      parseFailure = fail "Could not parse latitude/longitude"
+
 
 locationUrl = "https://ipinfo.io/json"
 
