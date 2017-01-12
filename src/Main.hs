b962262b18e65dae2d00d266d0836ad1985752ba
Sam Protas <smprts@gmail.com>
2017-01-12 01:09:51 -0500

    Add command-line parsing boilerplate

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,7 @@ import Configuration
 import Weather
 import Weather.WeatherUnderground
 
+import System.Console.ArgParser
 import System.Exit
 import Control.Exception.Safe
 import Control.Monad.Reader
@@ -18,12 +19,28 @@ import qualified Data.Text as T
 
 
 main :: IO ()
-main = handle
-  (\e -> die $ "Exited with: " ++ show (e :: SomeWeatherException))
-  (do
-    config <- getConfig
-    runWithConfig getWeather config
-    return ())
+main = do
+  interface <- getCmdLnInterface
+  runApp interface run
+
+run :: ParsedArgs ->  IO ()
+run args = handle
+            (\e -> die $ "Exited with: " ++ show (e :: SomeWeatherException))
+            (do
+              config <- getConfig
+              runWithConfig getWeather config
+              return ())
+
 
 getWeather :: WeatherAppIO (SpecificConfig WeatherUndergroundApiKey) ()
 getWeather = getLocation >>= getConditions >>= displayWeather
+
+type ReportType = String
+data ParsedArgs = ParsedArgs ReportType
+
+myWeatherParser :: ParserSpec ParsedArgs
+myWeatherParser = ParsedArgs
+  `parsedBy` optPos "current" "reportType" `Descr` "what specifically about the weather do you want to know?"
+
+getCmdLnInterface :: IO (CmdLnInterface ParsedArgs)
+getCmdLnInterface = pure $ mkDefaultApp myWeatherParser "weather"
